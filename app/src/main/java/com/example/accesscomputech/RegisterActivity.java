5271f209e6cad3aa61e5fbe7659534384b23fccf
Ashish Vyas <17ucs039@lnmiit.ac.in>
2020-01-31 02:14:27 +0530

    Login and register activity fixed

--- app/src/main/java/com/example/accesscomputech/RegisterActivity.java
+++ app/src/main/java/com/example/accesscomputech/RegisterActivity.java
@@ -1,14 +1,32 @@
 package com.example.accesscomputech;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
+    Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        register = findViewById(R.id.reg);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (register.getText().equals("Generate OTP")) {
+                    register.setText("Register");
+                    findViewById(R.id.otp_layout).setVisibility(View.VISIBLE);
+                }
+                else{
+                    startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+                }
+            }
+        });
     }
 }
